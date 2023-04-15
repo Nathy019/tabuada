ddf2133257a377cc5afb5225b106ef5103185e04
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-04-14 21:47:49 -0300

    finalizando tabuada

--- tabuada/Form1.Designer.cs
+++ tabuada/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.listBoxTabu = new System.Windows.Forms.ListBox();
             this.lblValorBuada = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -98,12 +99,22 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(339, 373);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.lblValorBuada);
             this.Controls.Add(this.listBoxTabu);
@@ -125,6 +136,7 @@
         private System.Windows.Forms.ListBox listBoxTabu;
         private System.Windows.Forms.Label lblValorBuada;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
