8806d671fa781cb1324e6e7b5ea8c1ae6b42f027
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-04-14 21:34:17 -0300

    Adicionar arquivos de projeto.

---
+++ tabuada/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace tabuada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.listBoxTabu = new System.Windows.Forms.ListBox();
+            this.lblValorBuada = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(252, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "mostrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(55, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite um valor para encotrar a tabuada";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(58, 38);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(191, 20);
+            this.txtValor.TabIndex = 2;
+            // 
+            // listBoxTabu
+            // 
+            this.listBoxTabu.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxTabu.FormattingEnabled = true;
+            this.listBoxTabu.ItemHeight = 16;
+            this.listBoxTabu.Location = new System.Drawing.Point(56, 74);
+            this.listBoxTabu.Name = "listBoxTabu";
+            this.listBoxTabu.Size = new System.Drawing.Size(175, 260);
+            this.listBoxTabu.TabIndex = 3;
+            // 
+            // lblValorBuada
+            // 
+            this.lblValorBuada.AutoSize = true;
+            this.lblValorBuada.Location = new System.Drawing.Point(94, 167);
+            this.lblValorBuada.Name = "lblValorBuada";
+            this.lblValorBuada.Size = new System.Drawing.Size(0, 13);
+            this.lblValorBuada.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(252, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(339, 373);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblValorBuada);
+            this.Controls.Add(this.listBoxTabu);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.ListBox listBoxTabu;
+        private System.Windows.Forms.Label lblValorBuada;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ tabuada/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ValorDigitado;
+            ValorDigitado = Convert.ToInt32(txtValor.Text);
+            for(int i = 0; i <=10; i++)
+            {
+               listBoxTabu.Items.Add(i + "X" + ValorDigitado + "=" + (i * ValorDigitado));
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBoxTabu.Items.Clear();
+            txtValor.Clear();
+            txtValor.Focus();
+        }
+    }
+}
